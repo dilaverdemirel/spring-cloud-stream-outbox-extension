18048dfdd6339a4a2265f18bb4810dad467ce271
Dilaver Demirel <dilaverdemirel@gmail.com>
2022-01-17 12:13:16 +0300

    delete query is marked @Modifying

--- src/main/java/com/dilaverdemirel/spring/outbox/repository/OutboxMessageRepository.java
+++ src/main/java/com/dilaverdemirel/spring/outbox/repository/OutboxMessageRepository.java
@@ -4,6 +4,7 @@ import com.dilaverdemirel.spring.outbox.domain.OutboxMessage;
 import com.dilaverdemirel.spring.outbox.domain.OutboxMessageStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -22,6 +23,7 @@ public interface OutboxMessageRepository extends CrudRepository<OutboxMessage, S
     @Query("select m from OutboxMessage as m where m.createdAt <= :delayStart and m.status = 'NEW'")
     Page<OutboxMessage> findMessagesThatCouldNotBeSent(@Param("delayStart") LocalDateTime delayStart, Pageable pageRequest);
 
+    @Modifying
     @Query("delete from OutboxMessage as m where m.createdAt >= :thresholdDate")
     long deleteOldOutboxMessages(@Param("thresholdDate") LocalDateTime thresholdDate);
 }
